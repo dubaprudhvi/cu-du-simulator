3a37a9320be9de9d5fe324b2de229cd5e6f494a1
dubaprudhvi <dubaprudhvi2000@gmail.com>
2025-11-18 18:16:27 +0530

    Updated M-plane Fault Management

--- mplane/fault/mplanefault.py
+++ mplane/fault/mplanefault.py
@@ -36,27 +36,7 @@ class NotificationService(notification_service_pb2_grpc.NotificationService):
         context.add_callback(on_rpc_done)
         
         return send_notifications()
-    
-    # def SendNotification(self, request, context):
-    #     """Handle single notification requests"""
-    #     try:
-    #         print(f"Received notification - Type: {request.type}, Payload: {request.payload}")
-    #         return notification_service_pb2.NotificationResponse(
-    #             success=True,
-    #             error_message=""
-    #         )
-    #     except Exception as e:
-    #         return notification_service_pb2.NotificationResponse(
-    #             success=False,
-    #             error_message=str(e)
-    #         )
-    
-    # def random_fault_payload(self):
-    #     payloadone = "{\"affected-objects\":[{\"name\":\"Sync Module\"}],\"event-time\":\"2025-11-13T11:39:24+00:00\",\"fault-id\":17,\"fault-severity\":\"WARNING\",\"fault-source\":\"Module\",\"fault-text\":\"Alarm is cleared\",\"hostname\":\"bluerobin\",\"is-cleared\":true,\"ru_id\":\"0\"}"
-    #     payloadtwo = "{\"affected-objects\":[{\"name\":\"Sync Module\"}],\"event-time\":\"2025-11-13T11:38:32+00:00\",\"fault-id\":17,\"fault-severity\":\"MAJOR\",\"fault-source\":\"Module\",\"fault-text\":\"Sync:No external sync source\",\"hostname\":\"bluerobin\",\"is-cleared\":false,\"ru_id\":\"0\"}"
-        
-    #     return payloadone if random.choice([True, False]) else payloadtwo
-
+ 
     def generate_fault_payload(self):
         
         fault_id = random.choice(config['MPLANEFAULT']['FAULT_ID_LIST'])
